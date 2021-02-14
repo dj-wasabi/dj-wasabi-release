9b7d9612566f0149af00f3a686407b691350d1f6
Werner Dijkerman <werner@dj-wasabi.nl>
2021-02-14 09:19:49 +0100

    Added a forgotten step to activate the Python version

--- run-me.py
+++ run-me.py
@@ -23,7 +23,7 @@ def get_args():
     return parser.parse_args()
 
 
-def getConfigScript(scripts=[], script=None):
+def getConfigScript(scripts: list = [], script: str = None) -> dict:
     """ Get the configuration for provided script.
     """
     for entry in scripts:
