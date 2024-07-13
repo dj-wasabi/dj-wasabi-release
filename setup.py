9609567bd78a8dc5182b6e95c60ac7cce68e223d
Werner Dijkerman <werner@dj-wasabi.nl>
2024-07-13 12:34:10 +0200

    Attempt to properly version it

--- setup.py
+++ setup.py
@@ -1,23 +1,7 @@
 import setuptools
 import os
 
-if 'CURRENT_BRANCH' in os.environ:
-    current_branch = os.environ["CURRENT_BRANCH"]
-else:
-    current_branch = 'main'
-
-if 'GITHUB_RUN_ID' in os.environ:
-    run_id = os.environ['GITHUB_RUN_ID']
-
-if 'CURRENT_TAG' in os.environ:
-    current_tag = os.environ["CURRENT_TAG"]
-    _new_tag = current_tag.split('.')
-    new_tag = "{m}.{i}.{p}-{c}".format(m=_new_tag[0], i=_new_tag[1], p=int(_new_tag[2]) + 1, c=run_id)
-
-if current_branch != "main":
-    latest_tag = new_tag
-else:
-    latest_tag = current_tag
+version = "0.4.22"
 
 with open("README.md", "r", encoding = "utf-8") as fh:
     long_description = fh.read()
@@ -25,7 +9,7 @@ with open("README.md", "r", encoding = "utf-8") as fh:
 setuptools.setup(
   name = 'dj-wasabi',
   packages = ['djWasabi'],
-  version = latest_tag,
+  version = version,
   license='MIT',
   description = 'My personal PIP package',
   long_description = 'My personal PIP package',
@@ -33,7 +17,7 @@ setuptools.setup(
   author = 'Werner Dijkerman',
   author_email = 'iam@werner-dijkerman.nl',
   url = 'https://github.com/dj-wasabi/dj-wasabi-release',
-  download_url = 'https://github.com/dj-wasabi/dj-wasabi-release/archive/{u}.tar.gz'.format(u=latest_tag),
+  download_url = 'https://github.com/dj-wasabi/dj-wasabi-release/archive/{u}.tar.gz'.format(u=version),
   keywords = ['personal'],
   package_dir={'': 'lib'},
   install_requires=[
